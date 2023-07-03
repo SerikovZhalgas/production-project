f12643209c54aba26c130609fca435fd449d1e30
zhalgas <zhalgasserikov@gmail.com>
2023-07-04 00:09:42 +0600

    feat: add webpack-bundle-analyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', { markupOnly: true, code: 100 }],
         'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,11 +2,10 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
-export function buildPlugins(
-    { paths, isDev }: BuildOptions,
-): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions,): webpack.WebpackPluginInstance[] {
     return [
         new HtmlWebpackPlugin({
             template: paths.html,
@@ -21,5 +20,8 @@ export function buildPlugins(
         }),
         new ReactRefreshWebpackPlugin(),
         new webpack.HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false
+        }),
     ];
 }
