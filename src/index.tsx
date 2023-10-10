749d1ac42a79affec3c394a09b66736a7f347a9f
SerikovZhalgas <zhalgasserikov@gmail.com>
2023-10-10 09:39:12 +0600

    add redux store, add first entry

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import App from './app/App';
+import { StoreProvider } from 'app/providers/StoreProvider';
 
+import App from './app/App';
 import 'shared/config/i18n/i18n';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
