9cf109840361bee03b74ac8c2a585cb3154dfefc
SerikovZhalgas <zhalgasserikov@gmail.com>
2023-10-23 11:03:26 +0600

    feat: article list, article item, skeleton and stories

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,7 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
-import { Article, ArticleList, ArticleView } from 'entities/Article';
+import { ArticleList, ArticleView } from 'entities/Article';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
