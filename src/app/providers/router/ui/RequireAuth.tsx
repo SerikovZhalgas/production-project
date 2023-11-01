ebfe9c895299f3b24faf42b430133b08f98893df
SerikovZhalgas <zhalgasserikov@gmail.com>
2023-11-01 17:12:45 +0600

    feat: add private route with required roles

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -1,14 +1,38 @@
 import { useSelector } from 'react-redux';
-import { getUserAuthData } from 'entities/User';
+import { getUserAuthData, getUserRoles, UserRole } from 'entities/User';
 import { Navigate, useLocation } from 'react-router-dom';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { useMemo } from 'react';
 
-export function RequireAuth({ children }: {children: JSX.Element}) {
+interface RequireAuthProps {
+    children: JSX.Element;
+    roles?: UserRole[];
+}
+export function RequireAuth(props: RequireAuthProps) {
+    const {
+        roles,
+        children,
+    } = props;
     const auth = useSelector(getUserAuthData);
+    const userRoles = useSelector(getUserRoles);
     const location = useLocation();
+
+    const hasRequiredRoles = useMemo(() => {
+        if (!roles) {
+            return true;
+        }
+
+        return roles.some((requiredRole) => {
+            return userRoles?.includes(requiredRole);
+        });
+    }, [roles, userRoles]);
 
     if (!auth) {
         return <Navigate to={RoutePath.main} state={{ from: location }} replace />;
+    }
+
+    if (!hasRequiredRoles) {
+        return <Navigate to={RoutePath.forbidden} state={{ from: location }} replace />;
     }
 
     return children;
