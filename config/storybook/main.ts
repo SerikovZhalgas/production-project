05107c583ae31d6d27effd785be5fe9d70d8ab86
SerikovZhalgas <zhalgasserikov@gmail.com>
2023-11-29 16:17:23 +0600

    feat: upgrade storybook, fix packages

---
+++ config/storybook/main.ts
@@ -0,0 +1,70 @@
+import path from 'path';
+import { DefinePlugin } from 'webpack';
+import { Configuration } from 'mini-css-extract-plugin';
+import { buildCssLoader } from '../build/loaders/buildCssLoader';
+import { BuildPaths } from '../build/types/config';
+
+export default {
+    stories: [
+        '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                backgrounds: false,
+            },
+        },
+        '@storybook/addon-interactions',
+        'storybook-addon-mock/register',
+        'storybook-addon-themes',
+    ],
+    framework: '@storybook/react',
+    core: {
+        builder: '@storybook/builder-webpack5',
+    },
+    webpackFinal: async (config: Configuration) => {
+        const paths: BuildPaths = {
+            build: '',
+            html: '',
+            entry: '',
+            src: path.resolve(__dirname, '..', '..', 'src'),
+            locales: '',
+            buildLocales: '',
+        };
+        config!.resolve!.modules = [
+            path.resolve(__dirname, '../../src'),
+            'node_modules',
+        ];
+        config!.resolve!.extensions!.push('.ts', '.tsx');
+        config.resolve!.alias = {
+            ...config.resolve!.alias,
+            '@': paths.src,
+        };
+
+        // eslint-disable-next-line no-param-reassign
+        // @ts-ignore
+        config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+            if (/svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
+
+            return rule;
+        });
+
+        config!.module!.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        config!.module!.rules.push(buildCssLoader(true));
+
+        config!.plugins!.push(new DefinePlugin({
+            __IS_DEV__: JSON.stringify(true),
+            __API__: JSON.stringify('http://testapi.ru'),
+            __PROJECT__: JSON.stringify('storybook'),
+        }));
+
+        return config;
+    },
+};
